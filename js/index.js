5c5a0f474bbd25e81bfade4e9d043c8da178c7d9
Barros Lucas Javier <lucasbcco@gmail.com>
2025-01-10 19:36:51 -0300

    version mobile terminada

--- js/index.js
+++ js/index.js
@@ -36,11 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
         items[itemActivo].classList.add('activo');
         miniaturas[itemActivo].classList.add('activo');   
 
-        let limite = miniaturas[itemActivo].getBoundingClientRect().y;
-        if (limite < 600 && limite > 400) {
-            miniaturas[itemActivo].scrollIntoView({ behavior: 'smooth', inline: 'center' });
-        }
-
         clearInterval(intervaloRefresco);
         intervaloRefresco = setInterval(()=>{
             sig.click();
@@ -57,28 +52,62 @@ document.addEventListener('DOMContentLoaded', () => {
     /* tema */
     
     const btnTema = document.querySelector('#btnTema')
-    const btnBarras = document.querySelector('#btnBarras');
-    const btnCerrar = document.querySelector('#btnCerrar');
     const body = document.querySelector('#body');
     
     btnTema.addEventListener('click',function(){
         body.classList.toggle('temaSecundario');
     })
-    btnBarras.addEventListener('click',function (){
-        menu.classList.toggle('mostrar');
-        
-    })
-    /* Menu */
-    const menu = document.querySelector('.menu');
-    const nav = document.getElementById('menuUl');
-    console.log(nav);
-    
-    btnCerrar.addEventListener('click',function (){
-        menu.classList.toggle('mostrar');
-        
-    })
-    nav.addEventListener('click',()=>{
-        menu.classList.toggle('mostrar');
-    })
+    /* menu activo */
+    const menuOptions = document.querySelectorAll('.menuItem');
+    const callback = (entries, observer)=>{
+        entries.forEach(entry =>{
+            if (entry.isIntersecting) {
+                menuOptions.forEach((option)=>{
+                    const soloDireccion = option.href.substring(menuOptions[0].href.indexOf('#') + 1);
+                    
+                    if (soloDireccion == entry.target.id) {
+                        option.classList.add('activo')
+                    } else {
+                        option.classList.remove('activo')
+                    }
+                })
+            }
+        });
+    }
+    const options = {
+        root:null,
+        rootMargin:'0px',
+        threshold:0.7
+    }
+    const observer = new IntersectionObserver(callback,options);
 
+    const sections = document.querySelectorAll('section')
+    sections.forEach(section => observer.observe(section))
+
+    /* correo */
+    const formulario = document.getElementById('formContacto');
+    const mensaje = document.getElementById('mensaje');
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+
+    formulario.addEventListener('submit',(e)=>{
+        e.preventDefault();
+
+        const params = {
+            user_name: nombre.value,
+            user_email: email.value,
+            message: mensaje.value
+        }
+
+        emailjs.send('service_byid9rp','template_i898a0a', params ).then(function(response){
+            console.log('Enviado !!', response.status, response.text);
+            nombre.value = '';
+            email.value = '';
+            mensaje.value = '';
+        },function(error){
+            console.log('Error', error);
+        })
+        
+        
+    });
 });
